55ef8840c381de7eafe84eb9a6cf2a64793c1c9c
Erik Eldridge <erikeldridge@gmail.com>
2020-09-15 23:47:05 -0700

    Rebuild JS files after changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,19 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
 
+// Copies static assets to build dir
 function copy() {
   return gulp.src([
     'src/*.html',
-    'src/*.css',
-    'src/*.js'
+    'src/*.css'
   ])
   .pipe(gulp.dest('build'));
 }
 
+// Serves from build dir and refreshes browser on changes
 function serve() {
   return browserSync.init({
     server: 'build',
@@ -18,6 +22,27 @@ function serve() {
   });
 }
 
+// Runs JS source through Babel
+function processJs() {
+  return gulp.src('src/*.js')
+  .pipe(babel({
+      presets: ['@babel/env']
+  }))
+  .pipe(uglify())
+  .pipe(rename({
+    suffix: '.min'
+  }))
+  .pipe(gulp.dest('build'));
+}
+
+// Rebuilds JS on changes
+function watch() {
+  gulp.watch('src/*.js', processJs);
+}
+
+gulp.task('watch', watch);
+gulp.task('processJs', processJs);
 gulp.task('copy', copy);
-gulp.task('buildAndServe', gulp.series(copy,serve));
+gulp.task('buildAndServe', gulp.series(copy,processJs,
+  gulp.parallel(serve, watch)));
 
